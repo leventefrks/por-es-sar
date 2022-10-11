73c97d1ea27fde415b7c07c8e79ac89008db03e1
x-farkasle <x-farkasle@wizzair.com>
2022-10-11 22:30:39 +0200

    profile

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -43,7 +43,7 @@ export default MobileMenu;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 2rem;
+  gap: 2.2rem;
   background-color: var(--color-background);
   top: 60px;
   inset: 0;
