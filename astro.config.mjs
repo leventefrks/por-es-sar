7a417fbc5a556cd6487e8efacc2d80bf28ed843d
x-farkasle <x-farkasle@wizzair.com>
2022-11-30 22:47:17 +0100

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
   server: {
     port: 4000,
-    host: true,
+    host: true
   },
+  output: "server",
+  adapter: netlify()
 });
