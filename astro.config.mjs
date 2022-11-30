7759af5233473d7f6b84a203297b2f3cf003c53c
x-farkasle <x-farkasle@wizzair.com>
2022-11-30 21:39:47 +0100

    removed purge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import purgecss from 'astro-purgecss';
 import htmlMinifier from 'astro-html-minifier';
 
 // https://astro.build/config
@@ -8,9 +7,6 @@ export default defineConfig({
   integrations: [
     vue(),
     purgecss({
-      fontFace: true,
-      keyframes: true,
-    }),
   ],
   server: {
     port: 4000,
